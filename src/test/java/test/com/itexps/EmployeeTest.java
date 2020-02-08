2ab7a15f01d8db449db4b83ae9bfff5c314a6955
anugbo <57274149+anugbo@users.noreply.github.com>
2020-02-08 16:51:37 -0600

    Add files via upload

---
+++ src/test/java/test/com/itexps/EmployeeTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com.itexps;
+
+import com.itexps.Employee;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author okoob
+ */
+public class EmployeeTest {
+    Employee emp;
+    
+    public EmployeeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        emp = new Employee();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void testEmployee() {
+     emp.setId(1);
+     emp.setFirst ("tom");
+     emp.setEmail("austin@abc.com");
+     
+     Assert.assertEquals(1, emp.getId());  
+     Assert.assertEquals("tom", emp.getFirst());  
+     Assert.assertEquals("austin@abc.com", emp.getEmail());  
+     
+     }
+}
